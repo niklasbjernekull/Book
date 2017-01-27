a4d1abb37e21662ef532fd60b4e2889baf1158bd
niklasbjernekull <niklasbjernekull@hotmail.com>
2017-01-27 10:54:27 +0100

    Initial commit

---
+++ app/src/main/java/com/minichi/book/common/SharedResources.java
@@ -0,0 +1,13 @@
+package com.minichi.book.common;
+
+import android.content.Context;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by Niklas.bjernekull on 2017-01-27.
+ */
+
+public class SharedResources {
+    public static Context context;
+    public static FragmentActivity activity;
+}
