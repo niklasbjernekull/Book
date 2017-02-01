91b715bc4e75fe28b75e2c35783249e0b47837e1
niklasbjernekull <niklasbjernekull@hotmail.com>
2017-02-01 10:24:20 +0100

    Test to get latest code

--- app/src/main/java/com/minichi/book/MainActivity.java
+++ app/src/main/java/com/minichi/book/MainActivity.java
@@ -1,13 +1,60 @@
 package com.minichi.book;
 
+import android.graphics.Bitmap;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import com.minichi.book.common.SharedResources;
+import com.minichi.book.tools.ImageHandler;
+
+public class MainActivity extends FragmentActivity {
+
+    private ImageView backgroundImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        SharedResources.context = getApplicationContext();
+
         setContentView(R.layout.activity_main);
+        backgroundImage = (ImageView) findViewById(R.id.background_image);
+
+        SharedResources.activity = this;
+
+        hideActivityBar();
+
+        setBackgroundImage();
+
+        setContentView(R.layout.main_menu);
+    }
+
+    /*@Override
+    protected void onDestroy() {
+    }
+
+    @Override
+    public void onBackPressed() {
+    }*/
+
+    private void hideActivityBar() {
+        final int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+
+            getWindow().getDecorView().setSystemUiVisibility(flags);
+    }
+
+    private void setBackgroundImage() {
+        Bitmap bitmap = ImageHandler.scaleResource(R.drawable.cow_background, ImageHandler.screenWidth(), ImageHandler.screenHeight());
+        //bitmap = ImageHandler.crop(bitmap, ImageHandler.screenHeight(), ImageHandler.screenWidth());
+        //bitmap = ImageHandler.scaleBitmap(bitmap, 2);
+        backgroundImage.setImageBitmap(bitmap);
     }
 }
